92fc33fb9c637489d5743061dbf6df6fe2dc34e4
ppouke <sakkepouket@gmail.com>
2021-03-11 14:15:54 +0200

    Create models.py

---
+++ models.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+from sqlalchemy.exc import IntegrityError, OperationalError
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+class Message(db.Model):
+    __tablename__ = 'messages'
+    serverID = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String, nullable = False)
+    user = db.Column(db.String, nullable = False)
+
+
+class Server(db.Model):
+    __tablename__ = 'servers'
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String, nullable = False)
